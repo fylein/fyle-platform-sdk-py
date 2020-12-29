60bce9e05e0e8183f5e2c8cc7d2bd2880f4f926e
Shreyansh <shreyansh@noob.local>
2020-12-29 20:10:45 +0530

    Bug fix for files endpoint

--- fyle/platform/apis/v1/admin/files.py
+++ fyle/platform/apis/v1/admin/files.py
@@ -7,9 +7,9 @@ from ....internals.get_resources import GetResources
 
 
 class Files(PostResources, GetResources):
-    """Class for Orgs APIs."""
+    """Class for Files APIs."""
 
     FILES = '/files'
 
     def __init__(self, version, role):
-        super().__init__(version, role, Files.ORGS)
+        super().__init__(version, role, Files.FILES)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='fyle',
-    version='0.1.8-beta',
+    version='0.1.9-beta',
     author='Siva Narayanan',
     author_email='siva@fyle.in',
     description='Python SDK for accessing Fyle Platform APIs',
