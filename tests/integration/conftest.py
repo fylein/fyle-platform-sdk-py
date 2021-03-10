ea9a683684ac16468d8f10868a4ab265c6f549e8
jayachandhar <jayachandhar.jr@fyle.in>
2021-03-10 23:40:29 +0530

    simplify tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,49 +1,8 @@
-import os
-
 import pytest
 
-from tests.common import fyle_connect, get_admin_schema, get_approver_schema, get_common_schema, get_fyler_schema
-
-
-@pytest.fixture
-def admin_schema():
-    return get_admin_schema()
-
-
-@pytest.fixture
-def fyler_schema():
-    return get_fyler_schema()
-
-
-@pytest.fixture
-def approver_schema():
-    return get_approver_schema()
-
-
-@pytest.fixture
-def common_schema():
-    return get_common_schema()
-
-
-def get_mock_server_url(module):
-    server_url = None
-    if module == 'integration.test_admin_apis':
-        server_url = os.getenv('ADMIN_APIS_URL')
-    elif module == 'integration.test_approver_apis':
-        server_url = os.getenv('APPROVER_APIS_URL')
-    elif module == 'integration.test_fyler_apis':
-        server_url = os.getenv('FYLER_APIS_URL')
-    elif module == 'integration.test_common_apis':
-        server_url = os.getenv('COMMON_APIS_URL')
-    print(server_url)
-    return server_url
+from tests.common import fyle_connect
 
 
 @pytest.fixture(scope='module')
-def fyle(request):
-    is_mock_server = os.getenv('IS_MOCK_SERVER', 'F')
-    if is_mock_server == 'T':
-        server_url = get_mock_server_url(request.module.__name__)
-    else:
-        server_url = os.getenv('SERVER_URL')
-    return fyle_connect(server_url)
+def fyle():
+    return fyle_connect()
--- tests/integration/test_admin_apis.py
+++ tests/integration/test_admin_apis.py
@@ -34,19 +34,6 @@ def test_get_employees_invalid(fyle, admin_schema):
     jsonschema.validate(instance=employees, schema=employees_schema)
 
 
-def test_get_expenses(fyle, admin_schema):
-    """
-    Test Fyle expenses get call
-    Parameters:
-        fyle (obj): Fyle SDK instance
-    """
-    expenses = fyle.v1.admin.expenses.list()
-    url = expenses.get('url')
-
-    expenses_schema = admin_schema[url]['get']['responses']['200']
-    jsonschema.validate(instance=expenses, schema=expenses_schema)
-
-
 def test_get_reports(fyle, admin_schema):
     """
     Test Fyle reports get call
@@ -58,45 +45,6 @@ def test_get_reports(fyle, admin_schema):
 
     reports_schema = admin_schema[url]['get']['responses']['200']
     jsonschema.validate(instance=reports, schema=reports_schema)
-
-
-def test_get_balance_transfers(fyle, admin_schema):
-    """
-    Test Fyle balance_transfers get call
-    Parameters:
-        fyle (obj): Fyle SDK instance
-    """
-    balance_transfers = fyle.v1.admin.balance_transfers.list()
-    url = balance_transfers.get('url')
-
-    balance_transfers_schema = admin_schema[url]['get']['responses']['200']
-    jsonschema.validate(instance=balance_transfers, schema=balance_transfers_schema)
-
-
-def test_get_advance_requests(fyle, admin_schema):
-    """
-    Test Fyle advance requests get call
-    Parameters:
-        fyle (obj): Fyle SDK instance
-    """
-    advance_requests = fyle.v1.admin.advance_requests.list()
-    url = advance_requests.get('url')
-
-    advance_requests_schema = admin_schema[url]['get']['responses']['200']
-    jsonschema.validate(instance=advance_requests, schema=advance_requests_schema)
-
-
-def test_get_advances(fyle, admin_schema):
-    """
-    Test Fyle advances get call
-    Parameters:
-        fyle (obj): Fyle SDK instance
-    """
-    advances = fyle.v1.admin.advances.list()
-    url = advances.get('url')
-
-    advances_schema = admin_schema[url]['get']['responses']['200']
-    jsonschema.validate(instance=advances, schema=advances_schema)
 
 
 def test_get_categories(fyle, admin_schema):
