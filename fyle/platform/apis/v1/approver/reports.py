967e6ff60c78ecf468d07791fd9c8c5fa345784c
adi_fyle <aditya.agrawal@fyle.in>
2021-03-30 16:23:00 +0530

    init

--- fyle/platform/apis/v1/approver/reports.py
+++ fyle/platform/apis/v1/approver/reports.py
@@ -10,6 +10,19 @@ class Reports(ListResources, GetResources):
     """Class for Reports APIs."""
 
     REPORTS = '/reports'
+    PARTIALLY_APPROVE_REPORT = '/reports/{id}/partially_approve'
 
     def __init__(self, version, role):
         super().__init__(version, role, Reports.REPORTS)
+
+    def approve(self, id_):
+        """
+        To approve the report
+        :param id_: id of report to be approved
+        :return: partially_approved_report
+        """
+        partially_approve_report_url = Reports.PARTIALLY_APPROVE_REPORT.format(id=id_)
+        return self.api.make_post_request(
+            api_url=partially_approve_report_url,
+            payload={}
+        )
