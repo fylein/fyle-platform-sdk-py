8efd1ef510b51e8617416d8b2131a902c3b5e6a9
adi_fyle <aditya.agrawal@fyle.in>
2021-06-02 17:57:49 +0530

    minor

--- fyle/platform/apis/v1/approver/__init__.py
+++ fyle/platform/apis/v1/approver/__init__.py
@@ -3,8 +3,10 @@
 """
 
 from .reports import Reports
+from .subscriptions import Subscriptions
 from ..version import version
 
 role = 'approver'
 
 reports = Reports(version, role)
+subscriptions = Subscriptions(version, role)
--- fyle/platform/apis/v1/fyler/__init__.py
+++ fyle/platform/apis/v1/fyler/__init__.py
@@ -3,8 +3,10 @@
 """
 
 from .my_profile import MyProfile
+from .subscriptions import Subscriptions
 from ..version import version
 
 role = 'fyler'
 
 my_profile = MyProfile(version, role)
+subscriptions = Subscriptions(version, role)
