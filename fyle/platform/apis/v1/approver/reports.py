7627d391df028cc088e6bb9a6b625ee104a03c29
jayachandhar <jayachandhar.jr@fyle.in>
2021-06-19 14:24:54 +0530

    remove id from report approve API call (#10)

--- fyle/platform/apis/v1/approver/reports.py
+++ fyle/platform/apis/v1/approver/reports.py
@@ -2,15 +2,15 @@
 V1 Approver Reports
 """
 
-from ....internals.list_resources import ListResources
 from ....internals.get_resources import GetResources
+from ....internals.list_resources import ListResources
 
 
 class Reports(ListResources, GetResources):
     """Class for Reports APIs."""
 
     REPORTS = '/reports'
-    PARTIALLY_APPROVE_REPORT = '/reports/{id}/partially_approve'
+    PARTIALLY_APPROVE_REPORT = '/reports/partially_approve'
 
     def __init__(self, version, role):
         super().__init__(version, role, Reports.REPORTS)
@@ -21,8 +21,12 @@ class Reports(ListResources, GetResources):
         :param id_: id of report to be approved
         :return: partially_approved_report
         """
-        partially_approve_report_url = Reports.PARTIALLY_APPROVE_REPORT.format(id=id_)
+        partially_approve_report_url = Reports.PARTIALLY_APPROVE_REPORT
         return self.api.make_post_request(
             api_url=partially_approve_report_url,
-            payload={}
+            payload={
+                'data': {
+                    'id': id_
+                }
+            }
         )
