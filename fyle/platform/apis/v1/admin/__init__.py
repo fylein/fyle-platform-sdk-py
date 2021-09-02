c1d77a951ddd15895d2a0acb161111dc2f157c01
Shwetabh Kumar <shwetabh002@gmail.com>
2021-09-02 12:49:51 +0530

    Support for Tax Groups and Expenses APIs

--- fyle/platform/apis/v1/admin/__init__.py
+++ fyle/platform/apis/v1/admin/__init__.py
@@ -5,6 +5,8 @@
 from .categories import Categories
 from .cost_centers import CostCenters
 from .employees import Employees
+from .expenses import Expenses
+from .tax_groups import TaxGroups
 from .invitations import Invitations
 from .projects import Projects
 from ..version import version
@@ -16,3 +18,5 @@ cost_centers = CostCenters(version, role)
 employees = Employees(version, role)
 projects = Projects(version, role)
 invitations = Invitations(version, role)
+expenses = Expenses(version, role)
+tax_groups = TaxGroups(version, role)
