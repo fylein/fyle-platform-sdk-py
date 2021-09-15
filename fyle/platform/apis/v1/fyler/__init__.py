797ba9a0237177d789f706712d7d79d432210587
Shwetabh Kumar <shwetabh002@gmail.com>
2021-09-15 16:21:37 +0530

    Fyler APIs: Expense Fields(tentative), Cost Centers, Projects, Categories(tentative)

--- fyle/platform/apis/v1/fyler/__init__.py
+++ fyle/platform/apis/v1/fyler/__init__.py
@@ -3,8 +3,16 @@
 """
 
 from .my_profile import MyProfile
+from .cost_centers import CostCenters
+from .projects import Projects
+from .categories import Categories
+from .expense_fields import ExpenseFields
 from ..version import version
 
 role = 'fyler'
 
 my_profile = MyProfile(version, role)
+cost_centers = CostCenters(version, role)
+categories = Categories(version, role)
+projects = Projects(version, role)
+expense_fields = ExpenseFields(version, role)
