d384585d75cfe28933c10e6e59cebcc15f480324
Sravan Kumar <sravan.kumar@fyle.in>
2021-09-24 18:08:14 +0530

    Adding support for Files and Accounting Exports APIs "

--- fyle/platform/apis/v1/admin/__init__.py
+++ fyle/platform/apis/v1/admin/__init__.py
@@ -11,6 +11,8 @@ from .reimbursements import Reimbursements
 from .settlements import Settlements
 from .projects import Projects
 from .advance_requests import AdvanceRequests
+from .files import Files
+from .accounting_exports import AccountingExports
 from ..version import version
 
 role = 'admin'
@@ -24,3 +26,5 @@ tax_groups = TaxGroups(version, role)
 reimbursements = Reimbursements(version, role)
 settlements = Settlements(version, role)
 advance_requests = AdvanceRequests(version, role)
+files = Files(version, role)
+accounting_exports = AccountingExports(version, role)
