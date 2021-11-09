08be550596714d1587f05e8d7a10b258aeb38309
Ashutosh Muley <muleyashutosh@gmail.com>
2021-11-09 14:00:13 +0530

    quick fix

--- fyle/platform/apis/v1/common/expense_extract.py
+++ fyle/platform/apis/v1/common/expense_extract.py
@@ -14,26 +14,25 @@ class ExpenseExtract(PostResources):
     def __init__(self, version, role) -> None:
         super().__init__(version, role, ExpenseExtract.EXPENSE_EXTRACT)
 
-    def extract(self, params_object):
+    def extract(self, file_name, b64_content):
         """
         To extract the expenses from images
         :param params_object
         :return extracted expense
         """
-        params_object = {} if params_object is None else params_object
-        if not params_object.get('file_name') or not params_object.get('b64_content'):
+        if not file_name or not b64_content:
             raise exceptions.WrongParamsError(
                 "Mandatory query params missing: file_name, b64_content are mandatory query params.")
 
-        if not len(params_object.get('file_name').split('.')) > 1:
+        if not len(file_name.split('.')) > 1:
             raise exceptions.WrongParamsError(
                 "Invalid Filename: Please add file extension")
 
         return self.post({
             "data": {
                 "files": [{
-                    "name": params_object['file_name'],
-                    'content': params_object['b64_content']
+                    "name": file_name,
+                    'content': b64_content
                 }]
             }
         })
