0c0e96d550a7cf7e4b51b2f781bd819578a689aa
Irfan Alam <irfan.alam@fyle.in>
2021-11-16 13:11:41 +0530

    support for approver projects added

--- fyle/platform/apis/v1/approver/__init__.py
+++ fyle/platform/apis/v1/approver/__init__.py
@@ -3,8 +3,10 @@
 """
 
 from .reports import Reports
+from .projects import Projects
 from ..version import version
 
 role = 'approver'
 
 reports = Reports(version, role)
+projects = Projects(version, role)
