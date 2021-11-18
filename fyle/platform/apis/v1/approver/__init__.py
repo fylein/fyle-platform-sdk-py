f3f4e8f77db74dd43e6c31f278ade0964c1515b0
Kirti Gautam <53394284+KirtiGautam@users.noreply.github.com>
2021-11-18 11:45:44 +0530

    Support for approver categories (#29)

--- fyle/platform/apis/v1/approver/__init__.py
+++ fyle/platform/apis/v1/approver/__init__.py
@@ -4,9 +4,11 @@
 
 from .reports import Reports
 from .projects import Projects
+from .categories import Categories
 from ..version import version
 
 role = 'approver'
 
 reports = Reports(version, role)
 projects = Projects(version, role)
+categories = Categories(version, role)
