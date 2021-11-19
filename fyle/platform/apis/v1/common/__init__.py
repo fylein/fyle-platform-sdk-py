6ba8a3a6493ac7229bf64a3da58cf343a2f4887d
Kirti Gautam <kirti.gautam@fyle.in>
2021-11-19 10:09:36 +0530

    Support for common/places

--- fyle/platform/apis/v1/common/__init__.py
+++ fyle/platform/apis/v1/common/__init__.py
@@ -3,11 +3,13 @@
 """
 
 from .currencies import Currencies
+from .places import Places
 from .expense_extract import ExpenseExtract
 from ..version import version
 
 
 role = 'common'
 
+places = Places(version, role)
 currencies = Currencies(version, role)
 expense_extract = ExpenseExtract(version, role)
---
+++ fyle/platform/apis/v1/common/places.py
@@ -0,0 +1,36 @@
+"""
+V1 Common Places
+"""
+
+from typing import Dict
+from ....internals.get_resources import GetResources
+from .... import exceptions
+
+
+class Places(GetResources):
+    """Class for Places APIs."""
+
+    PLACES = "/places"
+
+    def __init__(self, version, role):
+        super().__init__(version, role, Places.PLACES)
+
+    def get_by_id(self, id_) -> Dict:
+        api_url = self.endpoint
+
+        if not id_:
+            raise exceptions.WrongParamsError("Invalid Parameters")
+
+        query_params = {"id": id_}
+        response = self.api.make_get_request(
+            api_url=api_url,
+            query_params=query_params,
+        )
+
+        count = len(response["data"])
+        if id_ and count < 1:
+            raise exceptions.NotFoundItemError("Not found item with ID")
+        elif id_ and count > 1:
+            raise exceptions.MultipleObjectReturned("Multiple Objects returned")
+
+        return response.get("data")[0]
