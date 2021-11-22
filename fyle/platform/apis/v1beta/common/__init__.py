51b0331d99668a754b13034fa3dffb93afa289c1
Irfan Alam <irfan.alam@fyle.in>
2021-11-22 19:47:44 +0530

    common currencies exchange_rate support added (#33)

--- fyle/platform/apis/v1beta/common/__init__.py
+++ fyle/platform/apis/v1beta/common/__init__.py
@@ -3,6 +3,7 @@
 """
 
 from .currencies import Currencies
+from .currencies import CurrenciesExchangeRate
 from .expense_extract import ExpenseExtract
 from ..version import version
 
@@ -10,4 +11,5 @@ from ..version import version
 role = 'common'
 
 currencies = Currencies(version, role)
+currencies_exchange_rate = CurrenciesExchangeRate(version, role)
 expense_extract = ExpenseExtract(version, role)
--- fyle/platform/apis/v1beta/common/currencies.py
+++ fyle/platform/apis/v1beta/common/currencies.py
@@ -2,8 +2,10 @@
 V1 Beta Common Currencies
 """
 
-from ....internals.list_resources import ListResources
+from typing import Dict
 
+from ....internals.list_resources import ListResources
+from ....internals.get_resources import GetResources
 
 class Currencies(ListResources):
     """Class for Currencies APIs."""
@@ -21,4 +23,29 @@ class Currencies(ListResources):
         return self.api.make_get_request(
             api_url=self.endpoint,
             query_params={}
+        )
+
+
+class CurrenciesExchangeRate(GetResources):
+    """Class for Currencies Exchange Rate APIs."""
+
+    CURRENCIES_EXCHANGE_RATE = "/currency/exchange_rate"
+
+    def __init__(self, version, role):
+        super().__init__(version, role, CurrenciesExchangeRate.CURRENCIES_EXCHANGE_RATE)
+
+    def get(self, from_, to_, date) -> Dict:
+        """
+        To get object of currencies exchange rate
+        :return: currencies exchange rate
+        """
+        query_params = {
+            'from': from_,
+            'to': to_,
+            'date': date
+        }
+
+        return self.api.make_get_request(
+            api_url=self.endpoint,
+            query_params=query_params
         )
