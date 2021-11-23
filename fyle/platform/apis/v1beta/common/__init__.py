3adb1e13f191093978df30d9f9e7a53afb7c6c5d
Kirti Gautam <53394284+KirtiGautam@users.noreply.github.com>
2021-11-23 06:16:33 +0530

    Support for common/places (#31)

--- fyle/platform/apis/v1beta/common/__init__.py
+++ fyle/platform/apis/v1beta/common/__init__.py
@@ -5,11 +5,13 @@
 from .currencies import Currencies
 from .currencies import CurrenciesExchangeRate
 from .expense_extract import ExpenseExtract
+from .places import Places
 from ..version import version
 
 
 role = 'common'
 
+places = Places(version, role)
 currencies = Currencies(version, role)
 currencies_exchange_rate = CurrenciesExchangeRate(version, role)
 expense_extract = ExpenseExtract(version, role)
