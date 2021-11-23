bdc8156cf8a5d93f6c08b84fd9e320b9f76003a0
Kirti Gautam <kirti.gautam@fyle.in>
2021-11-23 10:35:07 +0530

    Support for places autocomplete

--- fyle/platform/apis/v1beta/common/__init__.py
+++ fyle/platform/apis/v1beta/common/__init__.py
@@ -5,12 +5,13 @@
 from .currencies import Currencies
 from .currencies import CurrenciesExchangeRate
 from .expense_extract import ExpenseExtract
-from .places import Places
+from .places import Places, PlacesAutocomplete
 from ..version import version
 
 
 role = 'common'
 
+placesAutocomplete = PlacesAutocomplete(version, role)
 places = Places(version, role)
 currencies = Currencies(version, role)
 currencies_exchange_rate = CurrenciesExchangeRate(version, role)
