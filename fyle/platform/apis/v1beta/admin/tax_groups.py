d57e352a42ebe849e607ba434cd5ba3e835e6218
Nilesh Pant <nileshpant6969@gmail.com>
2022-01-11 22:31:26 +0530

    Added Support for bulk of Tax Group

--- fyle/platform/apis/v1beta/admin/tax_groups.py
+++ fyle/platform/apis/v1beta/admin/tax_groups.py
@@ -2,16 +2,19 @@
 V1 Beta Admin Tax Groups
 """
 
+from fyle.platform.internals.post_bulk_resources import PostBulkResources
 from ....internals.get_resources import GetResources
 from ....internals.list_all_resources import ListAllResources
 from ....internals.list_resources import ListResources
 from ....internals.post_resources import PostResources
 
 
-class TaxGroups(ListResources, ListAllResources, PostResources, GetResources):
+class TaxGroups(ListResources, ListAllResources, PostResources, GetResources, PostBulkResources):
     """Class for Tax Groups APIs."""
 
     TAX_GROUPS = '/tax_groups'
+    BULK_TAX_GROUPS = '/tax_groups/bulk'
 
     def __init__(self, version, role):
         super().__init__(version, role, TaxGroups.TAX_GROUPS)
+        super().__init__(version, role, TaxGroups.BULK_TAX_GROUPS)
