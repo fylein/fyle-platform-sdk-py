75e6e940a23ffa06d0385ca9669a442e8ae34077
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2022-03-07 19:51:42 +0530

    added

--- fyle/platform/apis/v1beta/spender/expenses.py
+++ fyle/platform/apis/v1beta/spender/expenses.py
@@ -3,12 +3,19 @@ V1 Beta Spender Expenses
 """
 from ....internals.list_all_resources import ListAllResources
 from ....internals.list_resources import ListResources
+from ....internals.post_resources import PostResources
 
 
-class Expenses(ListResources, ListAllResources):
+class Expenses(ListResources, ListAllResources, PostResources):
     """Class for Expense APIs."""
 
     EXPENSES = '/expenses'
 
     def __init__(self, version, role):
         super().__init__(version, role, Expenses.EXPENSES)
+
+    def create_expense(self, payload):
+        self.api.make_post_request(
+            api_url=Expenses.EXPENSES,
+            payload=payload
+        )
