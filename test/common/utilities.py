009d745a35c79bc131e9cfae195ee30238e97eba
harshithaputtaswamy <harshithaputtaswamy>
2022-03-17 15:45:17 +0530

    tests for apis under approver, code coverage 100% for the same

--- test/common/utilities.py
+++ test/common/utilities.py
@@ -17,6 +17,10 @@ def dict_compare_keys(d1, d2, key_path=''):
     return res
   if not isinstance(d1, dict):
     return res
+  if not d2:
+    return res
+  if not isinstance(d2, dict):
+    return res
   for k in d1:
     if k not in d2:
       missing_key_path = f'{key_path}->{k}'
@@ -26,7 +30,7 @@ def dict_compare_keys(d1, d2, key_path=''):
         key_path1 = f'{key_path}->{k}'
         res1 = dict_compare_keys(d1[k], d2[k], key_path1)
         res = res + res1
-      elif isinstance(d1[k], list):
+      elif isinstance(d1[k], list) and isinstance(d2[k], list):
         key_path1 = f'{key_path}->{k}[0]'
         dv1 = d1[k][0] if len(d1[k]) > 0 else None
         dv2 = d2[k][0] if len(d2[k]) > 0 else None
