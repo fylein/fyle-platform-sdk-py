8bd22cca93ae64702fc3a2fda75ed3c24275ecac
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2022-04-12 00:01:04 +0530

    added

--- fyle/platform/apis/v1beta/common/__init__.py
+++ fyle/platform/apis/v1beta/common/__init__.py
@@ -4,7 +4,7 @@
 
 from .currencies import Currencies
 from .currencies import CurrenciesExchangeRate
-from .expense_extract import ExpenseExtract
+# from .expense_extract import ExpenseExtract
 from .places import Places, PlacesAutocomplete
 from ..version import version
 
@@ -15,4 +15,4 @@ places_autocomplete = PlacesAutocomplete(version, role)
 places = Places(version, role)
 currencies = Currencies(version, role)
 currencies_exchange_rate = CurrenciesExchangeRate(version, role)
-expense_extract = ExpenseExtract(version, role)
+# expense_extract = ExpenseExtract(version, role)
