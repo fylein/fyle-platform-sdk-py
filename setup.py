639e8a6b238f0f144e9ab1d2bcc1a63cabb8cbae
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2022-04-12 11:33:05 +0530

    Added support for GET of spender corporate cards API  (#52)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='fyle',
-    version='v0.23.0',
+    version='v0.24.0',
     author='Siva Narayanan',
     author_email='siva@fyle.in',
     description='Python SDK for accessing Fyle Platform APIs',
