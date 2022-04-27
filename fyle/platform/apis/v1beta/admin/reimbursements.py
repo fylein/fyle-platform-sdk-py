bb35e5318bbcb01b181458447a0aa9a4bd4bda19
Nilesh Pant <nileshpant6969@gmail.com>
2022-04-27 16:30:02 +0530

    minor fixes

--- fyle/platform/apis/v1beta/admin/reimbursements.py
+++ fyle/platform/apis/v1beta/admin/reimbursements.py
@@ -8,18 +8,17 @@ from ....internals.list_resources import ListResources
 from ....internals.post_resources import PostResources
 
 
-
 class Reimbursements(ListResources, ListAllResources, PostResources, GetResources):
     """Class for Reimbursements APIs."""
 
     REIMBURSEMENTS = '/reimbursements'
-    POST_REIMBURSEMENTS = '/reimbursements/mark_paid/bulk'
+    BULK_POST_REIMBURSEMENTS = '/reimbursements/mark_paid/bulk'
 
     def __init__(self, version, role):
         super().__init__(version, role, Reimbursements.REIMBURSEMENTS)
     
     def bulk_post_reimbursements(self, payload):
         return self.api.make_post_request(
-            api_url=Reimbursements.POST_REIMBURSEMENTS,
+            api_url=Reimbursements.BULK_POST_REIMBURSEMENTS,
             payload=payload
         )
