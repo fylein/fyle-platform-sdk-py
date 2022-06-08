d97b27b2150b2b6fb6fb4503c8e8149d1845ae3c
ashwin1111 <winash1998@gmail.com>
2022-06-08 13:57:15 +0530

    Extend get by id for admin expense fields

--- fyle/platform/apis/v1beta/admin/expense_fields.py
+++ fyle/platform/apis/v1beta/admin/expense_fields.py
@@ -4,9 +4,10 @@ V1 Beta Admin Expense Fields
 from ....internals.list_all_resources import ListAllResources
 from ....internals.list_resources import ListResources
 from ....internals.post_resources import PostResources
+from ....internals.get_resources import GetResources
 
 
-class ExpenseFields(ListResources, ListAllResources, PostResources):
+class ExpenseFields(ListResources, ListAllResources, PostResources, GetResources):
     """Class for Expense Fields APIs."""
 
     EXPENSE_FIELDS = '/expense_fields'
