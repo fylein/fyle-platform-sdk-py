e4f86cb513db8558ed9100adbbeaa0d686f151ab
harshithaputtaswamy <harshitha.p@fyle.in>
2022-11-17 17:55:37 +0530

    fixed broken tests

--- test/integration/approver/test_approver_apis.py
+++ test/integration/approver/test_approver_apis.py
@@ -15,19 +15,17 @@ def test_list_all_categories(fyle, mock_data):
   # Get a list of all Categories in a paginated manner and add to a list
   categories = []
   query_params = {
-    'order': 'created_at.desc'
+    'order': 'created_at.desc',
+    'limit': 100,
+    'offset': 10
   }
 
-  categories_generator = fyle.v1beta.approver.categories.list_all(query_params=query_params)
+  categories_generator = fyle.v1beta.approver.categories.list(query_params=query_params)
   mock_categories = mock_data.categories.get()
 
-  for response in categories_generator:
-    if response.get('data'):
-      categories.extend(response['data'])
-
-  if categories:
-    assert dict_compare_keys(categories[0], mock_categories[0]) == [], 'fyle.v1beta.approver.categories.list_all() has stuff that mock_data doesnt'
-    assert dict_compare_keys(mock_categories[0], categories[0]) == [], 'mock_data.categories.get() has stuff that fyle doesnt'
+  if categories_generator['data']:
+    assert dict_compare_keys(categories_generator['data'][0], mock_categories[0]) == [], 'fyle.v1beta.approver.categories.list_all() has stuff that mock_data doesnt'
+    assert dict_compare_keys(mock_categories[0], categories_generator['data'][0]) == [], 'mock_data.categories.get() has stuff that fyle doesnt'
 
 
 def test_list_reports(fyle, mock_data):
@@ -53,6 +51,7 @@ def test_list_reports(fyle, mock_data):
       if reports["state"] == "APPROVER_PENDING":
         report_id = reports["id"]
         break
+    print(reports_generator["data"][0])
     assert dict_compare_keys(reports_generator["data"][0], mock_reports[0]) == [], 'fyle.v1beta.approver.reports.list() has stuff that mock_data doesnt'
     assert dict_compare_keys(mock_reports[0], reports_generator["data"][0]) == [], 'mock_data.reports.get() has stuff that fyle doesnt'
 
@@ -73,5 +72,6 @@ def test_report_get_by_id(fyle, mock_data):
   reports_generator = fyle.v1beta.approver.reports.get_by_id(id_=report_id)
   mock_reports = mock_data.reports.get()
   if reports_generator:
+    print(reports_generator["data"])
     assert dict_compare_keys(reports_generator["data"], mock_reports[0]) == [], 'fyle.v1beta.approver.reports.list() has stuff that mock_data doesnt'
     assert dict_compare_keys(mock_reports[0], reports_generator["data"]) == [], 'mock_data.reports.get() has stuff that fyle doesnt'
