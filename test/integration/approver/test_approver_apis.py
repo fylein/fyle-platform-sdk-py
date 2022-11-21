ed27d3fe2c98cd87c81007a96204914f58cfd2b9
harshithaputtaswamy <44926484+harshithaputtaswamy@users.noreply.github.com>
2022-11-21 12:03:47 +0530

    added support for department api (#70)
    * added support for deartment api
    
    * added tests for admin departments api
    
    * new line
    
    * fixed broken tests
    
    * added new line
    
    * version update

--- test/integration/approver/test_approver_apis.py
+++ test/integration/approver/test_approver_apis.py
@@ -15,19 +15,17 @@ def test_list_all_categories(fyle, mock_data):
   # Get a list of all Categories in a paginated manner and add to a list
   categories = []
   query_params = {
-    'order': 'created_at.desc'
+    'order': 'created_at.desc',
+    'limit': 100,
+    'offset': 10
   }
 
-  categories_generator = fyle.v1beta.approver.categories.list_all(query_params=query_params)
+  categories_generator = fyle.v1beta.approver.categories.list(query_params=query_params)
   mock_categories = mock_data.categories.get()
 
-  for response in categories_generator:
-    if response.get('data'):
-      categories.extend(response['data'])
-
-  if categories:
-    assert dict_compare_keys(categories[0], mock_categories[0]) == [], 'fyle.v1beta.approver.categories.list_all() has stuff that mock_data doesnt'
-    assert dict_compare_keys(mock_categories[0], categories[0]) == [], 'mock_data.categories.get() has stuff that fyle doesnt'
+  if categories_generator['data']:
+    assert dict_compare_keys(categories_generator['data'][0], mock_categories[0]) == [], 'fyle.v1beta.approver.categories.list_all() has stuff that mock_data doesnt'
+    assert dict_compare_keys(mock_categories[0], categories_generator['data'][0]) == [], 'mock_data.categories.get() has stuff that fyle doesnt'
 
 
 def test_list_reports(fyle, mock_data):
