02e1d62de8533e5cf969f2ab55bbf1e3983262fb
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2023-03-01 22:45:18 +0530

    Added Support For Dependent Fields Values (#74)

--- fyle/platform/apis/v1beta/admin/expense_fields.py
+++ fyle/platform/apis/v1beta/admin/expense_fields.py
@@ -11,6 +11,20 @@ class ExpenseFields(ListResources, ListAllResources, PostResources, GetResources
     """Class for Expense Fields APIs."""
 
     EXPENSE_FIELDS = '/expense_fields'
+    DEPENDENT_EXPENSE_FIELDS_VALUES = '/dependent_expense_field_values'
+    BULK_CREATE_DEPENDENT_EXPENSE_FIELDS_VALUES = '/dependent_expense_field_values/bulk'
 
     def __init__(self, version, role):
         super().__init__(version, role, ExpenseFields.EXPENSE_FIELDS)
+
+
+    def get_dependent_expense_field_values(self):
+        return self.api.make_get_request(
+            api_url=ExpenseFields.DEPENDENT_EXPENSE_FIELDS_VALUES
+        )
+
+    def bulk_post_dependent_expense_field_values(self, payload):
+        return self.api.make_post_request(
+            api_url=ExpenseFields.BULK_CREATE_DEPENDENT_EXPENSE_FIELDS_VALUES,
+            payload=payload
+        )
