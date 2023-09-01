98f96840bfe5e76e9b6f4801fb1021f79906c853
ashwin1111 <winash1998@gmail.com>
2023-09-01 18:53:56 +0530

    Support Accounting Export Summary Bulk POST

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='fyle',
-    version='v0.32.0',
+    version='v0.33.0',
     author='Siva Narayanan',
     author_email='siva@fyle.in',
     description='Python SDK for accessing Fyle Platform APIs',
