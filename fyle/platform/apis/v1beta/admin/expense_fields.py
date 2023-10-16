1001066be8abd668ce54712fc120c83bbc0587ed
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2023-10-16 17:26:28 +0530

    Adding separate resource for Dependent Field Values

--- fyle/platform/apis/v1beta/admin/expense_fields.py
+++ fyle/platform/apis/v1beta/admin/expense_fields.py
@@ -11,24 +11,6 @@ class ExpenseFields(ListResources, ListAllResources, PostResources, GetResources
     """Class for Expense Fields APIs."""
 
     EXPENSE_FIELDS = '/expense_fields'
-    DEPENDENT_EXPENSE_FIELDS_VALUES = '/dependent_expense_field_values'
-    BULK_CREATE_DEPENDENT_EXPENSE_FIELDS_VALUES = '/dependent_expense_field_values/bulk'
 
     def __init__(self, version, role):
         super().__init__(version, role, ExpenseFields.EXPENSE_FIELDS)
-
-
-    def get_dependent_expense_field_values(self, expense_field_id: int, parent_expense_field_id: int):
-        return self.api.make_get_request(
-            api_url=ExpenseFields.DEPENDENT_EXPENSE_FIELDS_VALUES,
-            query_params={
-                'expense_field_id': f'eq.{expense_field_id}',
-                'parent_expense_field_id': f'eq.{parent_expense_field_id}'
-            }
-        )
-
-    def bulk_post_dependent_expense_field_values(self, payload):
-        return self.api.make_post_request(
-            api_url=ExpenseFields.BULK_CREATE_DEPENDENT_EXPENSE_FIELDS_VALUES,
-            payload=payload
-        )
