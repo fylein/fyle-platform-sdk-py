16adcb94411dd9aae5368e33f987edc74840ce55
Hrishabh Tiwari <hrishabh.t@fyle.in>
2026-07-16 12:18:00 +0530

    fix pypi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='fyle',
-    version='2.0.0',
+    version='1.4.0',
     author='Siva Narayanan',
     author_email='siva@fyle.in',
     description='Python SDK for accessing Fyle Platform APIs',
